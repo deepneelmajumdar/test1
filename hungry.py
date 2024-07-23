27b8540ee172c785886d58ab2a1c1a49a751f54e
deepneel <deepneelmajumdar@gmail.com>
2024-07-23 12:29:45 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input("are you hungry?")
 if hungry=="yes":
     print("eat lunch")
     print("eat snacks")
+    print("eat burger")
+    print("eat fries")
 else:
     print("do your work")
